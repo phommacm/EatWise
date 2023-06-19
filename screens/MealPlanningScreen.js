921754a7a9540a8d532bf2436f9752a9c0ed5b90
MelvynDao <melvyn.dao@edu.esiee.fr>
2023-06-19 23:20:46 +0200

    feat(meal-planner): adding the possibility to scroll up and down the list

--- screens/MealPlanningScreen.js
+++ screens/MealPlanningScreen.js
@@ -1,129 +1,132 @@
 // MealPlanningScreen.js
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 const MealPlanningScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>Monday</Text>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Breakfast</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>Monday</Text>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+          </View>
         </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
+
+        <View style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>Tuesday</Text>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+          </View>
         </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
+
+        <View style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>Wednesday</Text>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+          </View>
         </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Snack</Text>
+
+        <View style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>Thursday</Text>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+          </View>
+        </View>
+
+        <View style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>Friday</Text>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+          </View>
+        </View>
+
+        <View style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>Saturday</Text>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+          </View>
+        </View>
+
+        <View style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>Sunday</Text>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Breakfast</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Lunch</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Dinner</Text>
+          </View>
+          <View style={styles.mealContainer}>
+            <Text style={styles.mealTitle}>Snack</Text>
+          </View>
         </View>
       </View>
 
-      <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>Tuesday</Text>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Breakfast</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Snack</Text>
-        </View>
-      </View>
-
-      <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>Wednesday</Text>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Breakfast</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Snack</Text>
-        </View>
-      </View>
-
-      <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>Thursday</Text>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Breakfast</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Snack</Text>
-        </View>
-      </View>
-
-      <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>Friday</Text>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Breakfast</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Snack</Text>
-        </View>
-      </View>
-
-      <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>Saturday</Text>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Breakfast</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Snack</Text>
-        </View>
-      </View>
-
-      <View style={styles.dayContainer}>
-        <Text style={styles.dayTitle}>Sunday</Text>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Breakfast</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Lunch</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Dinner</Text>
-        </View>
-        <View style={styles.mealContainer}>
-          <Text style={styles.mealTitle}>Snack</Text>
-        </View>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     padding: 16,
   },
   dayContainer: {
